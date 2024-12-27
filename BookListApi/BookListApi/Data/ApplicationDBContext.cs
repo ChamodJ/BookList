716670818282036f76fd826447de91e3a0d77154
Chamod Perera <chamod914@gmail.com>
2024-12-27 23:55:43 +0530

    DB Context

---
+++ BookListApi/BookListApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using BookListApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Book> Book { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=CHAMOD;Initial Catalog=BookList;Integrated Security=True;Encrypt=False");
+        }
+
+    }
+}
