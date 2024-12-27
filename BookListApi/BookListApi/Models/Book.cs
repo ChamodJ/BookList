ede64a98763fc63728e8ce7753b36f46b25cbf62
Chamod Perera <chamod914@gmail.com>
2024-12-27 22:35:30 +0530

    Book model class

---
+++ BookListApi/BookListApi/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookListApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; } 
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public DateTime? publicationDate { get; set; }
+    }
+}
