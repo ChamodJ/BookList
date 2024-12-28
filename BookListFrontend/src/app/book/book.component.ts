073e33988d603cda7b622c8f9584fab44b9b823a
Chamod Perera <chamod914@gmail.com>
2024-12-28 14:40:01 +0530

    Update Book component

--- BookListFrontend/src/app/book/book.component.ts
+++ BookListFrontend/src/app/book/book.component.ts
@@ -1,11 +1,32 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-book',
-  imports: [],
+  imports: [
+    HttpClientModule
+  ],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
+  bookArray : any[] = []
+  isResultLoaded = false
+  isUpdateFormActive = false
 
+  //Form Data
+  bTitle : string = ""
+  bAuthor : string = ""
+  bISBN : string = ""
+  bPubDate : string = ""
+
+  currentBookId = ""
+
+  constructor(private http: HttpClient)
+  {
+    this.getAllBooks()
+  }
+
+  getAllBooks() {
+  }
 }
